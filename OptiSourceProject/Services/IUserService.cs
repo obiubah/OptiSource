b7072415b43061a951930d3012fbd5d8e34211c4
cubah <sewpabee@gmail.com>
2019-12-13 22:47:31 +0100

    * .gitignore: 1. Create boilerplate .NET Core API project 2. Added relevant entites and models 3. Deleted useless classes
    * OptiSourceProject.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * WeatherForecast.cs:
    * User.cs:
    * RegisterModel.cs:
    * OptiSourceProject.csproj:
    * IUserService.cs:
    * AuthenticateModel.cs:
    * appsettings.Development.json:
    * UserService.cs:
    * launchSettings.json:
    * WeatherForecastController.cs:

---
+++ OptiSourceProject/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using OptiSourceProject.Models;
+
+namespace OptiSourceProject.Services
+{
+    public interface IUserService
+    {
+        User Authenticate(string username, string password);
+        User Create(User user, string password);
+    }
+}
---
+++ OptiSourceProject/Services/Impl/UserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OptiSourceProject.Models;
+
+namespace OptiSourceProject.Services.Impl
+{
+    public class UserService : IUserService
+    {
+
+        public UserService(){
+
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Create(User user, string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
